8923bbfee55520af68c67a8f4a64baac58a078c0
Vini-Urias <76217672+Vini-Urias@users.noreply.github.com>
2023-05-16 07:57:09 -0300

    Delete Aula 9/Ex4 directory

--- Aula 9/Ex4/ExemploSobreposicao/Dois.java
+++
@@ -1,5 +0,0 @@
-public class Dois extends Um {
-    public int calc(int x){
-        return (x+5);
-    }
-}
--- Aula 9/Ex4/ExemploSobreposicao/Um.java
+++
@@ -1,5 +0,0 @@
-public class Um{
-    public int calc(int x){
-        return(x*x);
-    }
-}
